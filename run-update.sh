b17862ed1078bdc60ba1888222600004a5578123
martin.jahr@reeeliance.com <martin.jahr@reeeliance.com>
2020-05-22 01:57:09 +0200

    share

--- run-update.sh
+++ run-update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export PATH=/usr/local/texlive:$PATH
-ls -lR / | grep texlive
+export PATH=/usr/share/texlive:$PATH
 /home/runner/work/selfscrum/selfscrum/update-tlmgr-latest.sh -- --upgrade
 
